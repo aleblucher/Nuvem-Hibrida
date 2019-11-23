980a07ed4ef0e6992f5ab44a9707e09bf5df2a0c
aleblucher <alessandrablucher@hotmail.com>
2019-11-23 20:32:58 -0300

    userdata script tested and functional

--- script.py
+++ script.py
@@ -19,11 +19,11 @@ def createSecurityGroup():
 def createInstance():
     ec2r = boto3.resource('ec2')
 
-    ec2r.create_instances(
+    resp = ec2r.create_instances(
 
     ImageId='ami-04763b3055de4860b',
     InstanceType='t2.micro',
-    KeyName='banananana',
+    KeyName='2nd',
     MaxCount=1,
     MinCount=1,
 
@@ -31,7 +31,29 @@ def createInstance():
     SecurityGroups=[
         'banana_group',
     ],
-    UserData='userdata.sh'
-    )
+    UserData='''#!/bin/bash
+sudo apt -y update
 
-createInstance()
+sudo apt install language-pack-pt -y
+sudo apt install python3-pip -y
+git clone https://github.com/aleblucher/Nuvem-Hibrida
+sudo apt -y install python-pip python-dev
+sudo pip3 install flask
+cd Nuvem-Hibrida
+echo "instalando mysql"
+echo 'debconf debconf/frontend select Dialog' | sudo debconf-set-selections
+yes "" | sudo apt -y install mysql-server-core-5.7 << EOF
+
+
+
+
+EOF
+sudo mysql < ./scripts/123.sql
+sudo pip3 install requests
+sudo apt install tmux
+sudo tmux new -d -s execution 'export SERVER="http://localhost:5000"; sudo python3 ./database/connector.py'
+'''
+    )
+    return resp
+
+print(createInstance())
